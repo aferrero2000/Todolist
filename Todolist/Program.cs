b2fa3a8b152c67091bae3f6c44eb393929126687
dcastro <dcastro.dam@institutcampalans.net>
2021-11-03 15:44:27 +0100

    dcastro canvi

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -6,7 +6,7 @@ namespace Todolist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! DCASTRO");
         }
     }
 }
