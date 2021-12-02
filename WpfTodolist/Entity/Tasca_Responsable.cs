890c5d487db507f679af5e9760d2e366dec54e45
llucoliveras <loliveras.dam@institutcampalans.net>
2021-12-02 17:55:04 +0100

    Veure nom responsable a la tasca (MainWindow)

---
+++ WpfTodolist/Entity/Tasca_Responsable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfTodolist.Entity
+{
+    public class Tasca_Responsable
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Descripcio { get; set; }
+        public DateTime Data_creacio { get; set; }
+        public DateTime Data_finalitzacio { get; set; }
+        public string Responsable { get; set; }
+        public string Prioritat { get; set; }
+        public string Estat { get; set; }
+    }
+}
