d37fc1444ca2d0100f59c937d4b2173a0f070455
dcastro <dcastro.dam@institutcampalans.net>
2022-02-02 16:41:13 +0100

    Create ResponsableApiClient.cs

---
+++ Client/WpfTodolist/ApiClient/ResponsableApiClient.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WpfTodolist.Entity;
+
+namespace WpfTodolist.ApiClient
+{
+    class ResponsableApiClient
+    {
+        string BaseUri;
+
+        public ResponsableApiClient()
+        {
+            BaseUri = ConfigurationManager.AppSettings["BaseUri"];
+        }
+
+        /// <summary>
+        /// Obté un usuari a partir del Id
+        /// </summary>
+        /// <param name="Id">Codi d'usuari</param>
+        /// <returns>Usuari o null si no el troba</returns>
+        public async Task<List<Responsable>> GetResponsableAsync()
+        {
+            List<Responsable> listresponsable = new List<Responsable>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició GET al endpoint /users/{Id}
+                HttpResponseMessage response = await client.GetAsync($"responsable");
+                if (response.IsSuccessStatusCode)
+                {
+                    //Reposta 204 quan no ha trobat dades
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        listresponsable = null;
+                    }
+                    else
+                    {
+                        //Obtenim el resultat i el carreguem al Objecte User
+                        listresponsable = await response.Content.ReadAsAsync<List<Responsable>>();
+                        response.Dispose();
+                    }
+                }
+                else
+                {
+                    //TODO: que fer si ha anat malament? retornar null? 
+                }
+            }
+            return listresponsable;
+        }
+
+
+        /// <summary>
+        /// Obté un usuari a partir del Id
+        /// </summary>
+        /// <param name="Id">Codi d'usuari</param>
+        /// <returns>Usuari o null si no el troba</returns>
+        public async Task<List<Responsable>> GetResponsableIDAsync(int id)
+        {
+            List<Responsable> listresponsable = new List<Responsable>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició GET al endpoint /users/{Id}
+                HttpResponseMessage response = await client.GetAsync($"responsable/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    //Reposta 204 quan no ha trobat dades
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        listresponsable = null;
+                    }
+                    else
+                    {
+                        //Obtenim el resultat i el carreguem al Objecte User
+                        listresponsable = await response.Content.ReadAsAsync<List<Responsable>>();
+                        response.Dispose();
+                    }
+                }
+                else
+                {
+                    //TODO: que fer si ha anat malament? retornar null? 
+                }
+            }
+            return listresponsable;
+        }
+
+        /// <summary>
+        /// Afegeix un nou usuari
+        /// </summary>
+        /// <param name="user">Usuari que volem afegir</param>
+        /// <returns></returns>
+        public async Task AddResponsableAsync(Responsable responsable)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició POST al endpoint /users}
+                HttpResponseMessage response = await client.PostAsJsonAsync("responsable", responsable);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+
+        /// <summary>
+        /// Modificar un usuari
+        /// </summary>
+        /// <param name="user">Usuari que volem modificar</param>
+        /// <returns></returns>
+        public async Task UpdateResponsableAsync(Responsable responsable)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició PUT al endpoint /users/Id
+                HttpResponseMessage response = await client.PutAsJsonAsync($"users/{responsable.Id}", responsable);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+        /// <summary>
+        /// Modificar un usuari
+        /// </summary>
+        /// <param name="user">Usuari que volem modificar</param>
+        /// <returns></returns>
+        public async Task DeleteResponsableAsync(int Id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Enviem una petició DELETE al endpoint /users/Id
+                HttpResponseMessage response = await client.DeleteAsync($"responsable/{Id}");
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+    }
+}
